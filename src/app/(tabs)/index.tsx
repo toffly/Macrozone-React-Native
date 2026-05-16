79a821e4e7e0f730e3a67fd22c7d45b7b31798d8
toffly <toffly2@gmail.com>
2026-05-16 14:35:00 +0700

    first commit

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,33 @@
+import { Text, ScrollView } from "react-native";
+import { globalStyles } from "@/styles/style";
+import HomeHeader from "@/components/HomeHeader";
+import MacroGrid from "@/components/MacroGrid";
+import RecentMeal from "@/components/RecentMeals";
+import { useCallback, useState } from "react";
+import { getMeals, Meal } from "@/storage/meal";
+import { useFocusEffect } from "expo-router";
+
+export default function Index() {
+  const [meals, setMeals] = useState<Meal[]>([])
+
+  const loadMeals = async() => {
+    const data = await getMeals()
+      setMeals(data)
+      console.log('loaded meals:', data)
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      loadMeals()
+    }, [])
+  )
+  
+  return (
+    <ScrollView style={globalStyles.container}>
+      <Text style={globalStyles.title}>MacroZone</Text>
+      <HomeHeader />
+      <MacroGrid/>
+      <RecentMeal/>
+    </ScrollView>
+  );
+}
